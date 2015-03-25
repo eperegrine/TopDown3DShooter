0a101c50bd3b5e2402599148d736224c5dc0b069
EverlessDrop41 <tomfperegrine@googlemail.com>
2015-03-25 19:43:05 +0000

    Added Player Gun, Camera Follow, Door, Police Char

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+
+    public bool X, Y, Z;
+
+    public float XOffset, YOffset, ZOffset;
+
+	void Update () {
+        transform.position = new Vector3 (
+            (X ? target.position.x + XOffset : transform.position.x),
+            (Y ? target.position.y + YOffset : transform.position.y),
+            (Z ? target.position.z + ZOffset : transform.position.z));
+        //Use of conditional operator
+	}
+}
---
+++ Assets/Scripts/Player/PlayerGun.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerGun : MonoBehaviour {
+
+    public float rayLength = 10;
+    public Transform LaunchPosition;
+    public ParticleSystem particle;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = new Ray(LaunchPosition.position, transform.forward);
+            Debug.DrawRay(ray.origin, ray.direction, Color.red, rayLength);
+            RaycastHit RayHit;
+
+            particle.Emit(5);
+
+            if (Physics.Raycast(ray, out RayHit, rayLength))
+            {
+                Destroy(RayHit.collider.gameObject);
+            }
+        }
+    }
+
+}
